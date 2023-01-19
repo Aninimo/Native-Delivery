af0d47a13d41680c106ef9a541d3a2db6616f520
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-01-19 19:56:50 -0200

    Create index.tsx

---
+++ src/theme/index.tsx
@@ -0,0 +1,16 @@
+import { extendTheme } from 'native-base'
+
+export const THEME = extendTheme({
+    colors:{
+      green:{
+        500: '#40916C'
+      },
+      red:{
+        500: '#EF233C'
+      },
+      yellow:{
+        500: '#FFD60A',
+        600: '#FFC300'
+      }
+    }
+})
