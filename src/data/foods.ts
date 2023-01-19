93f1bfe92a00da58c0953b80119ddb62a5437fb7
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-01-19 19:51:04 -0200

    Create foods.ts

---
+++ src/data/foods.ts
@@ -0,0 +1,42 @@
+export const foods = [
+  {
+    id: '1',
+    name: 'Pizza',
+    image: 'https://pngimg.com/cdn-cgi/mirage/bf9d574b654d252d1c21614abba1d272188674e971542038ed7a63088e920a01/1280/https://pngimg.com/uploads/pizza/small/pizza_PNG44075.png',
+    price: 16.79,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere gravida libero, ut rutrum tellus imperdiet non. Duis fermentum consectetur consequat. Ut tincidunt tellus pharetra urna euismod, at pretium mi posuere.',
+    ingredients: [
+      'Wheat flour', 'tomato sauce', 'cheese', 'pepperoni'
+    ]
+  },
+  {
+    id: '2',
+    name: 'Hamburger',
+    image: 'https://pngimg.com/uploads/burger_sandwich/small/burger_sandwich_PNG96779.png',
+    price: 12.30,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere gravida libero, ut rutrum tellus imperdiet non. Duis fermentum consectetur consequat. Ut tincidunt tellus pharetra urna euismod, at pretium mi posuere.',
+    ingredients: [
+      'Bread', 'onion', 'tomato', 'lettuce', 'meat', 'cheese', 'mayonnaise', 'mustard', 'ketchup'
+    ]
+  },
+  {
+    id: 3,
+    name: 'Sandwich',
+    image: 'https://pngimg.com/uploads/sandwich/small/sandwich_PNG54.png',
+    price: 8.75,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere gravida libero, ut rutrum tellus imperdiet non. Duis fermentum consectetur consequat. Ut tincidunt tellus pharetra urna euismod, at pretium mi posuere.',
+    ingredients:[
+      'Bread', 'onion', 'tomato', 'lettuce', 'chicken', 'cheese'
+    ]
+  },
+  {
+    id: 4,
+    name: 'Pie',
+    image: 'https://pngimg.com/uploads/pie/small/pie_PNG60.png',
+    price: 11.4,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere gravida libero, ut rutrum tellus imperdiet non. Duis fermentum consectetur consequat. Ut tincidunt tellus pharetra urna euismod, at pretium mi posuere.',
+    ingredients: [
+      'wheat flour', 'salt', 'vegetable fat', 'cherries', 'sugar', 'butter'
+    ]
+  }
+]
